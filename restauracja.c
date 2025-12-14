7f0796b048ba8f7e27f5faa8a65ac8b943e9d802
Michal Gal <mgalezowski@gmail.com>
2025-12-14 18:17:42 +0100

    zmiany zmiennych

--- restauracja.c
+++ restauracja.c
@@ -10,244 +10,252 @@
 #include <fcntl.h>
 
 // Definicje stałych
-#define MAX_QUEUE 50
-#define MAX_BELT 100
-#define MAX_TABLES 10
+#define MAX_KOLEJKA 500
+#define MAX_TASMA 500
+#define MAX_STOLIKI 100
 
 // Struktury danych
 typedef struct Grupa
 {
-    int size;
-    int is_vip;
-    int has_children;
-    int adult_supervisors;
+    int wielkosc;
+    int VIP;
+    int dzieci;
+    int dorosli;
 } Grupa;
 
 typedef struct Talerz
 {
-    int price;
-    int for_table;
+    int cena;
+    int do_stolika;
 } Talerz;
 
 typedef struct Stolik
 {
-    int occupied;
-    int capacity;
+    int zajety;
+    int pojemnosc;
     Grupa grupa;
 } Stolik;
 
 typedef struct
 {
-    Grupa queue[MAX_QUEUE];
-    int front;
-    int rear;
-    int count;
+    Grupa kolejka[MAX_KOLEJKA];
+    int przod;
+    int tyl;
+    int licznik;
 } Kolejka;
 
 typedef struct
 {
-    Talerz belt[MAX_BELT];
-    int head;
-    int tail;
-    int count;
+    Talerz tasma[MAX_TASMA];
+    int przod;
+    int tyl;
+    int licznik;
 } Tasma;
 
 // Globalne zmienne - teraz współdzielone
-Kolejka *customer_queue;
-Tasma *belt;
-Stolik *tables;
-int *total_customers;
-int *vip_count;
-int *manager_signal;           // Sygnał kierownika
-sem_t *queue_sem;              // Semafor dla kolejki
-sem_t *belt_sem;               // Semafor dla taśmy
-sem_t *table_sem;              // Semafor dla stolików
-int P;                         // Maksymalna pojemność taśmy
-int X1, X2, X3, X4, N, Tp, Tk; // Zmienne konfiguracyjne
-FILE *report;                  // Plik raportu
+Kolejka *kolejka_klientow;
+Tasma *tasma;
+Stolik *stoly;
+int *wszyscy_klienci;
+int *vip_licznik;
+int *sygnal_kierownika; // Sygnał kierownika
+sem_t *kolejka_sem;     // Semafor dla kolejki
+sem_t *tasma_sem;       // Semafor dla taśmy
+sem_t *kolejka_sem;     // Semafor dla stolików
+int P;                  // Maksymalna pojemność taśmy
+int X1;                 // Liczba stolików jednoosobowych
+int X2;                 // Liczba stolików dwuosobowych
+int X3;                 // Liczba stolików trzyosobowych
+int X4;                 // Liczba stolików czteroosobowych
+int N;                  // Całkowita liczba miejsc
+int Tp;                 // Czas otwarcia restauracji
+int Tk;                 // Czas zamknięcia restauracji
+FILE *raport;           // Plik raportu
 
 // Funkcje pomocnicze - dostosuj do wskaźników
-void init_belt()
+void inicjuj_tasma()
 {
-    belt->head = 0;
-    belt->tail = 0;
-    belt->count = 0;
+    tasma->przod = 0;
+    tasma->tyl = 0;
+    tasma->licznik = 0;
 }
 
-void add_to_belt(Talerz p)
+void dodaj_do_tasma(Talerz p)
 {
-    sem_wait(belt_sem);
-    if (belt->count < P)
+    sem_wait(tasma_sem);
+    if (tasma->licznik < P)
     {
-        belt->belt[belt->tail] = p;
-        belt->tail = (belt->tail + 1) % MAX_BELT;
-        belt->count++;
+        tasma->tasma[tasma->tyl] = p;
+        tasma->tyl = (tasma->tyl + 1) % MAX_TASMA;
+        tasma->licznik++;
     }
-    sem_post(belt_sem);
+    sem_post(tasma_sem);
 }
 
-Talerz remove_from_belt()
+Talerz usun_z_tasma()
 {
     Talerz p = {0, -1};
-    sem_wait(belt_sem);
-    if (belt->count > 0)
+    sem_wait(tasma_sem);
+    if (tasma->licznik > 0)
     {
-        p = belt->belt[belt->head];
-        belt->head = (belt->head + 1) % MAX_BELT;
-        belt->count--;
+        p = tasma->tasma[tasma->przod];
+        tasma->przod = (tasma->przod + 1) % MAX_TASMA;
+        tasma->licznik--;
     }
-    sem_post(belt_sem);
+    sem_post(tasma_sem);
     return p;
 }
 
-void init_queue()
+void inicjuj_kolejka()
 {
-    customer_queue->front = 0;
-    customer_queue->rear = 0;
-    customer_queue->count = 0;
+    kolejka_klientow->przod = 0;
+    kolejka_klientow->tyl = 0;
+    kolejka_klientow->licznik = 0;
 }
 
-void enqueue(Grupa g)
+void zakolejkuj(Grupa g)
 {
-    sem_wait(queue_sem);
-    if (customer_queue->count < MAX_QUEUE)
+    sem_wait(kolejka_sem);
+    if (kolejka_klientow->licznik < MAX_KOLEJKA)
     {
-        customer_queue->queue[customer_queue->rear] = g;
-        customer_queue->rear = (customer_queue->rear + 1) % MAX_QUEUE;
-        customer_queue->count++;
+        kolejka_klientow->kolejka[kolejka_klientow->tyl] = g;
+        kolejka_klientow->tyl = (kolejka_klientow->tyl + 1) % MAX_KOLEJKA;
+        kolejka_klientow->licznik++;
     }
-    sem_post(queue_sem);
+    sem_post(kolejka_sem);
 }
 
-Grupa dequeue()
+Grupa wykolejkuj()
 {
     Grupa g = {0, 0, 0, 0};
-    sem_wait(queue_sem);
-    if (customer_queue->count > 0)
+    sem_wait(kolejka_sem);
+    if (kolejka_klientow->licznik > 0)
     {
-        g = customer_queue->queue[customer_queue->front];
-        customer_queue->front = (customer_queue->front + 1) % MAX_QUEUE;
-        customer_queue->count--;
+        g = kolejka_klientow->kolejka[kolejka_klientow->przod];
+        kolejka_klientow->przod = (kolejka_klientow->przod + 1) % MAX_KOLEJKA;
+        kolejka_klientow->licznik--;
     }
-    sem_post(queue_sem);
+    sem_post(kolejka_sem);
     return g;
 }
 
-void client_process()
+void klient_proces()
 {
     while (1)
     {
-        sleep(rand() % 10);
+        // sleep(rand() % 10);
+        sleep(1);
+        (*wszyscy_klienci)++;
         Grupa g;
-        g.size = rand() % 4 + 1;
-        g.is_vip = (rand() % 100 < 2) ? 1 : 0;
-        g.has_children = rand() % 2;
-        g.adult_supervisors = g.has_children ? rand() % 3 + 1 : 0;
+        g.wielkosc = rand() % 4 + 1;
+        g.VIP = (rand() % 100 < 2) ? 1 : 0;
+        g.dzieci = rand() % 2;
+        g.dorosli = g.dzieci ? rand() % 3 + 1 : 0;
 
-        if (g.is_vip)
+        if (g.VIP)
         {
-            (*vip_count)++;
-            fprintf(report, "VIP grupa %d osób przybyła.\n", g.size);
-            sem_wait(table_sem);
-            for (int i = 0; i < MAX_TABLES; i++)
+            (*vip_licznik)++;
+            fprintf(raport, "VIP grupa %d osób przybyła.\n", g.wielkosc);
+            sem_wait(kolejka_sem);
+            for (int i = 0; i < MAX_STOLIKI; i++)
             {
-                if (tables[i].occupied == 0 && tables[i].capacity >= g.size)
+                if (stoly[i].zajety == 0 && stoly[i].pojemnosc >= g.wielkosc)
                 {
-                    tables[i].occupied = g.size;
-                    tables[i].grupa = g;
+                    stoly[i].zajety = g.wielkosc;
+                    stoly[i].grupa = g;
                     break;
                 }
             }
-            sem_post(table_sem);
+            sem_post(kolejka_sem);
         }
         else
         {
-            enqueue(g);
-            fprintf(report, "Grupa %d osób w kolejce.\n", g.size);
+            zakolejkuj(g);
+            fprintf(raport, "Grupa %d osób w kolejce.\n", g.wielkosc);
         }
     }
 }
 
-void staff_process()
+void obsluga_proces()
 {
     while (1)
     {
-        if (customer_queue->count > 0)
+        if (kolejka_klientow->licznik > 0)
         {
-            Grupa g = dequeue();
-            int assigned = 0;
-            sem_wait(table_sem);
-            for (int i = 0; i < MAX_TABLES; i++)
+            Grupa g = wykolejkuj();
+            int dolacz = 0;
+            sem_wait(kolejka_sem);
+            for (int i = 0; i < MAX_STOLIKI; i++)
             {
-                if (tables[i].occupied == 0 && tables[i].capacity >= g.size)
+                if (stoly[i].zajety == 0 && stoly[i].pojemnosc >= g.wielkosc)
                 {
-                    tables[i].occupied = g.size;
-                    tables[i].grupa = g;
-                    assigned = 1;
-                    fprintf(report, "Grupa %d osób przy stoliku %d.\n", g.size, i);
+                    stoly[i].zajety = g.wielkosc;
+                    stoly[i].grupa = g;
+                    dolacz = 1;
+                    fprintf(raport, "Grupa %d osób przy stoliku %d.\n", g.wielkosc, i);
                     break;
                 }
             }
-            sem_post(table_sem);
-            if (!assigned)
+            sem_post(kolejka_sem);
+            if (!dolacz)
             {
-                fprintf(report, "Brak miejsca dla grupy %d osób.\n", g.size);
+                fprintf(raport, "Brak miejsca dla grupy %d osób.\n", g.wielkosc);
             }
         }
 
         int speed = 1;
-        if (*manager_signal == 1)
+        if (*sygnal_kierownika == 1)
             speed = 2;
-        else if (*manager_signal == 2)
+        else if (*sygnal_kierownika == 2)
             speed = 0.5;
 
         for (int i = 0; i < speed; i++)
         {
             Talerz p;
-            p.price = (rand() % 3 + 1) * 10;
-            p.for_table = -1;
-            add_to_belt(p);
-            fprintf(report, "Dodano talerzyk %d zł na taśmę.\n", p.price);
+            p.cena = (rand() % 3 + 1) * 10;
+            p.do_stolika = -1;
+            dodaj_do_tasma(p);
+            fprintf(raport, "Dodano talerzyk %d zł na taśmę.\n", p.cena);
         }
         sleep(1);
     }
 }
 
-void chef_process()
+void kucharz_proces()
 {
     while (1)
     {
-        sleep(5);
+        sleep(3);
         Talerz p;
-        p.price = (rand() % 3 + 4) * 10;
-        p.for_table = rand() % MAX_TABLES;
-        add_to_belt(p);
-        fprintf(report, "Specjalne danie %d zł dla stolika %d.\n", p.price, p.for_table);
+        p.cena = (rand() % 3 + 4) * 10;
+        p.do_stolika = rand() % MAX_STOLIKI;
+        dodaj_do_tasma(p);
+        fprintf(raport, "Specjalne danie %d zł dla stolika %d.\n", p.cena, p.do_stolika);
     }
 }
 
-void manager_process()
+void kierownik_proces()
 {
     while (1)
     {
-        sleep(20);
-        *manager_signal = rand() % 4;
-        if (*manager_signal == 3)
+        sleep(9);
+        *sygnal_kierownika = rand() % 4;
+        if (*sygnal_kierownika == 3)
         {
-            sem_wait(table_sem);
-            for (int i = 0; i < MAX_TABLES; i++)
+            sem_wait(kolejka_sem);
+            for (int i = 0; i < MAX_STOLIKI; i++)
             {
-                tables[i].occupied = 0;
+                stoly[i].zajety = 0;
             }
-            sem_wait(queue_sem);
-            customer_queue->count = 0;
-            sem_post(queue_sem);
-            sem_post(table_sem);
-            fprintf(report, "Sygnał 3: Wszyscy opuszczają restaurację.\n");
+            sem_wait(kolejka_sem);
+            kolejka_klientow->licznik = 0;
+            sem_post(kolejka_sem);
+            sem_post(kolejka_sem);
+            fprintf(raport, "Sygnał 3: Wszyscy opuszczają restaurację.\n");
         }
         else
         {
-            fprintf(report, "Sygnał kierownika: %d\n", *manager_signal);
+            fprintf(raport, "Sygnał kierownika: %d\n", *sygnal_kierownika);
         }
     }
 }
@@ -255,114 +263,114 @@ void manager_process()
 int main()
 {
     srand(time(NULL));
-    report = fopen("raport.txt", "w");
+    raport = fopen("raport.txt", "w");
 
     // Tworzenie współdzielonej pamięci
-    int shm_queue = shmget(IPC_PRIVATE, sizeof(Kolejka), IPC_CREAT | 0666);
-    int shm_belt = shmget(IPC_PRIVATE, sizeof(Tasma), IPC_CREAT | 0666);
-    int shm_tables = shmget(IPC_PRIVATE, sizeof(Stolik) * MAX_TABLES, IPC_CREAT | 0666);
+    int shm_kolejka = shmget(IPC_PRIVATE, sizeof(Kolejka), IPC_CREAT | 0666);
+    int shm_tasma = shmget(IPC_PRIVATE, sizeof(Tasma), IPC_CREAT | 0666);
+    int shm_stoly = shmget(IPC_PRIVATE, sizeof(Stolik) * MAX_STOLIKI, IPC_CREAT | 0666);
     int shm_total = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     int shm_vip = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     int shm_signal = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
 
-    customer_queue = (Kolejka *)shmat(shm_queue, NULL, 0);
-    belt = (Tasma *)shmat(shm_belt, NULL, 0);
-    tables = (Stolik *)shmat(shm_tables, NULL, 0);
-    total_customers = (int *)shmat(shm_total, NULL, 0);
-    vip_count = (int *)shmat(shm_vip, NULL, 0);
-    manager_signal = (int *)shmat(shm_signal, NULL, 0);
+    kolejka_klientow = (Kolejka *)shmat(shm_kolejka, NULL, 0);
+    tasma = (Tasma *)shmat(shm_tasma, NULL, 0);
+    stoly = (Stolik *)shmat(shm_stoly, NULL, 0);
+    wszyscy_klienci = (int *)shmat(shm_total, NULL, 0);
+    vip_licznik = (int *)shmat(shm_vip, NULL, 0);
+    sygnal_kierownika = (int *)shmat(shm_signal, NULL, 0);
 
     // Inicjalizacja semaforów
-    queue_sem = sem_open("/queue_sem", O_CREAT, 0644, 1);
-    belt_sem = sem_open("/belt_sem", O_CREAT, 0644, 1);
-    table_sem = sem_open("/table_sem", O_CREAT, 0644, 1);
+    kolejka_sem = sem_open("/kolejka_sem", O_CREAT, 0644, 1);
+    tasma_sem = sem_open("/tasma_sem", O_CREAT, 0644, 1);
+    kolejka_sem = sem_open("/kolejka_sem", O_CREAT, 0644, 1);
 
     // Inicjalizacja
-    init_belt();
-    init_queue();
-    *total_customers = 0;
-    *vip_count = 0;
-    *manager_signal = 0;
-    X1 = 5;
-    X2 = 3;
-    X3 = 2;
-    X4 = 1;
+    inicjuj_tasma();
+    inicjuj_kolejka();
+    *wszyscy_klienci = 0;
+    *vip_licznik = 0;
+    *sygnal_kierownika = 0;
+    X1 = 10;
+    X2 = 10;
+    X3 = 10;
+    X4 = 10;
     N = X1 + 2 * X2 + 3 * X3 + 4 * X4;
-    P = 50;
+    P = 1000;
     Tp = 10;
     Tk = 22;
 
     int idx = 0;
     for (int i = 0; i < X1; i++)
-        tables[idx++].capacity = 1;
+        stoly[idx++].pojemnosc = 1;
     for (int i = 0; i < X2; i++)
-        tables[idx++].capacity = 2;
+        stoly[idx++].pojemnosc = 2;
     for (int i = 0; i < X3; i++)
-        tables[idx++].capacity = 3;
+        stoly[idx++].pojemnosc = 3;
     for (int i = 0; i < X4; i++)
-        tables[idx++].capacity = 4;
+        stoly[idx++].pojemnosc = 4;
 
     // Tworzenie procesów
-    pid_t pid_client = fork();
-    if (pid_client == 0)
+    pid_t pid_klient = fork(); // Proces klienta
+    if (pid_klient == 0)
     {
-        client_process();
+        klient_proces();
         exit(0);
     }
 
-    pid_t pid_staff = fork();
-    if (pid_staff == 0)
+    pid_t pid_obsluga = fork();
+    if (pid_obsluga == 0)
     {
-        staff_process();
+        obsluga_proces();
         exit(0);
     }
 
-    pid_t pid_chef = fork();
-    if (pid_chef == 0)
+    pid_t pid_kucharz = fork();
+    if (pid_kucharz == 0)
     {
-        chef_process();
+        kucharz_proces();
         exit(0);
     }
 
-    pid_t pid_manager = fork();
-    if (pid_manager == 0)
+    pid_t pid_kierownik = fork();
+    if (pid_kierownik == 0)
     {
-        manager_process();
+        kierownik_proces();
         exit(0);
     }
 
     // Główny proces: symulacja przez 60 sekund
-    sleep(20);
+    sleep(300);
 
     // Zabij procesy potomne
-    kill(pid_client, SIGTERM);
-    kill(pid_staff, SIGTERM);
-    kill(pid_chef, SIGTERM);
-    kill(pid_manager, SIGTERM);
+    kill(pid_klient, SIGTERM);
+    kill(pid_obsluga, SIGTERM);
+    kill(pid_kucharz, SIGTERM);
+    kill(pid_kierownik, SIGTERM);
 
     // Podsumowanie
-    fprintf(report, "Podsumowanie: Taśma ma %d talerzyków.\n", belt->count);
+    fprintf(raport, "Podsumowanie: Taśma ma %d talerzyków.\n", tasma->licznik);
 
     // Czyszczenie
-    shmdt(customer_queue);
-    shmctl(shm_queue, IPC_RMID, NULL);
-    shmdt(belt);
-    shmctl(shm_belt, IPC_RMID, NULL);
-    shmdt(tables);
-    shmctl(shm_tables, IPC_RMID, NULL);
-    shmdt(total_customers);
+    shmdt(kolejka_klientow);
+    shmctl(shm_kolejka, IPC_RMID, NULL);
+    shmdt(tasma);
+    shmctl(shm_tasma, IPC_RMID, NULL);
+    shmdt(stoly);
+    shmctl(shm_stoly, IPC_RMID, NULL);
+    shmdt(wszyscy_klienci);
     shmctl(shm_total, IPC_RMID, NULL);
-    shmdt(vip_count);
+    shmdt(vip_licznik);
     shmctl(shm_vip, IPC_RMID, NULL);
     shmdt(signal);
     shmctl(shm_signal, IPC_RMID, NULL);
-    sem_close(queue_sem);
-    sem_unlink("/queue_sem");
-    sem_close(belt_sem);
-    sem_unlink("/belt_sem");
-    sem_close(table_sem);
-    sem_unlink("/table_sem");
+    sem_close(kolejka_sem);
+    sem_unlink("/kolejka_sem");
+    sem_close(tasma_sem);
+    sem_unlink("/tasma_sem");
+    sem_close(kolejka_sem);
+    sem_unlink("/kolejka_sem");
 
-    fclose(report);
+    fclose(raport);
     return 0;
 }
